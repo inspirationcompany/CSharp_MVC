80b9b33287c4b0fbdfeca81f9ed8a923816600c2
Robban Blomberg <robban_blomberg@hotmail.com>
2016-10-31 10:49:09 +0100

    First Commit

---
+++ TestProject/Controllers/MoviesController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TestProject.Models;
+using TestProject.viewModels;
+
+namespace TestProject.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Id=1,  Name = "TESTAR LITE filmNamn..." };
+
+            var customer = new List<Customer>
+            {
+                new Customer { Name="Person A" },
+                new Customer { Name="Person B" }
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customer
+            };
+
+            return View(viewModel);
+            //ViewData["Movie"] = movie;
+            //return View();
+
+            //return View(movie);
+
+            //return Content("Testar Content!");
+            //return HttpNotFound();
+            //return new EmptyResult();
+            //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
+        }
+
+        public ActionResult Edit (int id)
+        {
+            return Content("id=" + id);
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+
+            if (string.IsNullOrWhiteSpace(sortBy))
+                sortBy = "name";
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+            //return Content("id=" + id);
+        }
+
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1, 12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+    }
+}
